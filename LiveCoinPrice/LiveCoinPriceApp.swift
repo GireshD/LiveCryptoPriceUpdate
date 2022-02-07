b9c12ebd31aa79ea20c2e2431777ec74ae2cd5de
Giresh Dora <gireshkumar.dora@honeywell.com>
2022-02-07 19:34:03 +0530

    Initial Commit

---
+++ LiveCoinPrice/LiveCoinPriceApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveCoinPriceApp.swift
+//  LiveCoinPrice
+//
+//  Created by Giresh Dora on 07/02/22.
+//
+
+import SwiftUI
+
+@main
+struct LiveCoinPriceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
